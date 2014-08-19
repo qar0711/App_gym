0ec6e74af275859bfb4018551ebd8ab2e94b6704
qar0711 <joseqar@gmail.com>
2014-08-19 17:34:47 -0500

    controller new

---
+++ final/Proyecto/Controllers/PruebaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Proyecto.Controllers
+{
+    public class PruebaController : Controller
+    {
+        //
+        // GET: /Prueba/
+
+        public ActionResult Index()
+        {
+            ViewBag("cualquier texto");
+            return View();
+        }
+
+    }
+}
